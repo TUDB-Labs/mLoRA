1db591569d8a6734bf071fd54abbc82d899a15f7
yezhengmao <yezhengmaolove@gmail.com>
2023-08-30 19:44:16 +0800

    clear the legacy code * change when init random lora weight * can config different lora with different optimizer * fix save path

--- aspen/__init__.py
+++ aspen/__init__.py
@@ -3,8 +3,8 @@ from aspen.tokenizer import Tokenizer
 from aspen.model import LlamaModel, Linear, RMSNorm
 from aspen.modelargs import TokenizerArgs, LlamaModelArgs, MultiLoraBatchData, LoraBatchDataConfig
 from aspen.dataset import DataSet
-from aspen.modelloader import load_llama_7b_weight, load_llama_tf_weight
-from aspen.modelloader import load_random_lora_7b_weight
+from aspen.modelloader import load_llama_7b_weight
+from aspen.modelloader import load_llama_tf_weight
 from aspen.modelloader import save_lora_model
 
 __all__ = [
@@ -20,6 +20,5 @@ __all__ = [
     "convert_hf_to_pth",
     "load_llama_7b_weight",
     "load_llama_tf_weight",
-    "load_random_lora_7b_weight",
     "save_lora_model"
 ]
