6dd70e7f280cda628231dad6a775cb3e80a556e4
LianxinGao <503773453@qq.com>
2023-09-13 11:01:06 +0800

    support chinese

--- aspen/eval.py
+++ aspen/eval.py
@@ -1,30 +1,37 @@
-import re
 import nltk.translate.bleu_score as bleu
 from rouge import Rouge as en_rouge
 from rouge_chinese import Rouge as zh_rouge
+import jieba
+
+
+def is_contains_chinese(strs):
+    for _char in strs:
+        if '\u4e00' <= _char <= '\u9fa5':
+            return True
+    return False
 
 
 def calculate_ROUGE(generated_summary, reference_summary):
-    def is_contains_chinese(strs):
-        for _char in strs:
-            if '\u4e00' <= _char <= '\u9fa5':
-                return True
-        return False
-
     if is_contains_chinese(reference_summary):
         rouger = zh_rouge()
+        generated_summary = ' '.join(jieba.cut(generated_summary))
+        reference_summary = ' '.join(jieba.cut(reference_summary))
     else:
         rouger = en_rouge()
 
-    scores = rouger.get_scores(generated_summary,reference_summary)
+    scores = rouger.get_scores(generated_summary, reference_summary)
     rouge_1_score = scores[0]['rouge-1']['f']
     rouge_2_score = scores[0]['rouge-2']['f']
     rouge_l_score = scores[0]['rouge-l']['f']
     return {'rouge-1': rouge_1_score, 'rouge-2': rouge_2_score, 'rouge-l': rouge_l_score}
 
+
 def calculate_BLEU(generated_summary, reference_summary, n):
-    # n = 4, focus on the fluency of the sentence
     # Tokenize the generated summary and reference summary
+    if is_contains_chinese(reference_summary):
+        generated_summary = ' '.join(jieba.cut(generated_summary))
+        reference_summary = ' '.join(jieba.cut(reference_summary))
+
     generated_tokens = generated_summary.split()
     reference_tokens = reference_summary.split()
 
@@ -32,4 +39,4 @@ def calculate_BLEU(generated_summary, reference_summary, n):
     weights = [1.0 / n] * n  # Weights for n-gram precision calculation
     bleu_score = bleu.sentence_bleu([reference_tokens], generated_tokens, weights=weights)
 
-    return {"bleu-%s"%n: bleu_score}
+    return {"bleu-%s" % n: bleu_score}
--- tests/eval_test.py
+++ tests/eval_test.py
@@ -9,3 +9,15 @@ print(rouge_score)
 n = 2
 bleu_score = aspen_eval.calculate_BLEU(generated_summary, reference_summary, n)
 print(bleu_score)
+
+generated_summary = "刚刚发声，A股这种情况十分罕见！大聪明逆市抄底330亿，一篇研报引爆全球，市场逻辑生变？"
+reference_summary = """刚刚过去的这个月，美股总市值暴跌了将近6万亿美元（折合人民币超过40万亿），这背后的原因可能不仅仅是加息这么简单。
+最近瑞士信贷知名分析师Zoltan Polzsar撰写了一篇极其重要的文章，详细分析了现有世界秩序的崩坏本质以及美国和西方将要采取的应对策略。
+在该文中，Zoltan Polzsar直指美国通胀的本质和其长期性。同期，A股市场亦出现了大幅杀跌的情况。"""
+
+rouge_score = aspen_eval.calculate_ROUGE(generated_summary, reference_summary)
+print(rouge_score)
+
+n = 2
+bleu_score = aspen_eval.calculate_BLEU(generated_summary, reference_summary, n)
+print(bleu_score)
