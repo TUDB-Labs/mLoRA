a26d4df827273d937345680d6c7c503c92553988
yezhengmao <yezhengmaolove@gmail.com>
2023-10-25 16:11:49 +0800

    add test case (#79)

---
+++ tests/dispatcher_test.py
@@ -0,0 +1,45 @@
+from aspen.dispatcher import TrainTask
+
+import json
+import unittest
+
+from typing import List
+
+with open('config/finetune.json', 'r', encoding='utf8') as fp:
+    config = json.load(fp)
+
+
+class MockTokenizer:
+    def __init__(self):
+        self.pad_id_ = 0
+
+    def encode(self, text: str, bos: bool, eos: bool) -> List[int]:
+        return [0] * len(text)
+
+
+class TestDataSet(unittest.TestCase):
+    def test_load_dataset(self):
+        tasks: List[TrainTask] = []
+
+        for lora in config["lora"]:
+            task = TrainTask(MockTokenizer(),
+                             lora["name"],
+                             lora["data"],
+                             -1,
+                             lora["test_data"],
+                             lora["prompt"],
+                             lora["num_epochs"],
+                             lora["batch_size"],
+                             lora["micro_batch_size"],
+                             lora["test_batch_size"],
+                             config["cutoff_len"],
+                             config["group_by_length"])
+            task.load_data()
+            tasks.append(task)
+            self.assertEqual(len(task.train_token_data_), 2)
+
+        self.assertEqual(len(tasks), 3)
+
+
+if __name__ == '__main__':
+    unittest.main()
