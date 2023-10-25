fe1792a28d0debdfa75405486874e0ff21c00d82
李登淳 <mikecovlee@163.com>
2023-10-25 11:52:13 +0800

    Update loader_test.py

--- tests/loader_test.py
+++ tests/loader_test.py
@@ -21,7 +21,6 @@ class TestDataSet(unittest.TestCase):
     def test_load_dataset(self):
         dataset = DataSet(config, MockTokenizer())
         input_data = dataset.get_batch_data()
-        print(input_data.prompts_)
         self.assertEqual(len(input_data.prompts_), 12)
         for p in input_data.prompts_:
             if "Input" in p:
