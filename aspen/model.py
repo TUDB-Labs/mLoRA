1f3d844f86c08c1f1c322b853233ae26de55d619
李登淳 <mikecovlee@163.com>
2023-10-25 11:07:47 +0800

    fix lint error

--- aspen/model.py
+++ aspen/model.py
@@ -65,7 +65,9 @@ def repeat_kv(x: torch.Tensor, n_rep: int) -> torch.Tensor:
     batch_size, seq_len, n_kv_heads, head_dim = x.shape
     if n_rep == 1:
         return x
-    return x[:, :, :, None, :].expand(batch_size, seq_len, n_kv_heads, n_rep, head_dim).reshape(batch_size, seq_len, n_kv_heads * n_rep, head_dim)
+    return x[:, :, :, None, :].expand(
+        batch_size, seq_len, n_kv_heads, n_rep, head_dim).reshape(
+            batch_size, seq_len, n_kv_heads * n_rep, head_dim)
 
 
 class RMSNorm():
