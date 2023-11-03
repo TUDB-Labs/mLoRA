e542419a26d40156165026744779b5ef62c5f421
LianxinGao <503773453@qq.com>
2023-11-03 16:51:05 +0800

    with chatglm

--- ci_scripts.py
+++ ci_scripts.py
@@ -1,10 +1,10 @@
-from transformers import LlamaForCausalLM, LlamaTokenizer
+from transformers import LlamaForCausalLM, LlamaTokenizer, AutoTokenizer, AutoModel
 import torch
 from peft import PeftModel
 import sys
 
 
-def inference_with_finetuned_lora(base_model_name_or_path, lora_weights_path, prompt):
+def inference_llama(base_model_name_or_path, lora_weights_path, prompt):
     device = "cuda:0"
     tokenizer = LlamaTokenizer.from_pretrained(base_model_name_or_path)
     model = LlamaForCausalLM.from_pretrained(
@@ -18,10 +18,20 @@ def inference_with_finetuned_lora(base_model_name_or_path, lora_weights_path, pr
         lora_weights_path,
         torch_dtype=torch.float16,
     )
+    inference(tokenizer, model, device)
 
-    generation_config = model.generation_config
 
+def inference_chatglm2(base_model_name_or_path, lora_weights_path, prompt):
+    device = "cuda:0"
+    tokenizer = AutoTokenizer.from_pretrained(base_model_name_or_path, trust_remote_code=True)
+    model = AutoModel.from_pretrained(base_model_name_or_path, trust_remote_code=True).to(device)
+    model = PeftModel.from_pretrained(model, lora_weights_path)
+    inference(tokenizer, model, device)
+
+
+def inference(tokenizer, model, device):
     encoding = tokenizer(prompt, return_tensors="pt").to(device)
+    generation_config = model.generation_config
     with torch.inference_mode():
         outputs = model.generate(
             input_ids=encoding.input_ids,
@@ -37,8 +47,12 @@ def inference_with_finetuned_lora(base_model_name_or_path, lora_weights_path, pr
 if __name__ == '__main__':
     args = sys.argv
     method = args[1]
-    if method == 'inference':
-        base_model_or_path = args[2]
-        lora_weight_path = args[3]
-        prompt = args[4]
-        inference_with_finetuned_lora(base_model_or_path, lora_weight_path, prompt)
+    base_model_or_path = args[2]
+    lora_weight_path = args[3]
+    prompt = args[4]
+
+    if method == 'llama':
+        inference_llama(base_model_or_path, lora_weight_path, prompt)
+    elif method == 'chatglm':
+        inference_chatglm2(base_model_or_path, lora_weight_path, prompt)
+
