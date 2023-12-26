9c483858d235001c326cccddb2556340de75d84a
Michael Lee <mikecovlee@163.com>
2023-12-26 20:11:24 +0800

    support aux loss

--- mlora/MixLoRA.py
+++ mlora/MixLoRA.py
@@ -3,7 +3,56 @@ from mlora.LoraLiner import Linear
 
 import torch
 import torch.nn.functional as F
-from typing import Optional
+from typing import Tuple, Optional
+
+
+def router_z_loss_func(router_logits: torch.Tensor) -> float:
+    num_groups, tokens_per_group, _ = router_logits.shape
+    log_z = torch.logsumexp(router_logits, dim=-1)
+    z_loss = log_z**2
+    return torch.sum(z_loss) / (num_groups * tokens_per_group)
+
+
+def load_balancing_loss_func(router_probs: torch.Tensor, expert_indices: torch.Tensor) -> float:
+    num_experts = router_probs.shape[-1]
+
+    # cast the expert indices to int64, otherwise one-hot encoding will fail
+    if expert_indices.dtype != torch.int64:
+        expert_indices = expert_indices.to(torch.int64)
+
+    if len(expert_indices.shape) == 2:
+        expert_indices = expert_indices.unsqueeze(2)
+
+    expert_mask = torch.nn.functional.one_hot(expert_indices, num_experts)
+
+    # For a given token, determine if it was routed to a given expert.
+    expert_mask = torch.max(expert_mask, axis=-2).values
+
+    # cast to float32 otherwise mean will fail
+    expert_mask = expert_mask.to(torch.float32)
+    tokens_per_group_and_expert = torch.mean(expert_mask, axis=-2)
+
+    router_prob_per_group_and_expert = torch.mean(router_probs, axis=-2)
+    return torch.mean(tokens_per_group_and_expert * router_prob_per_group_and_expert) * (num_experts**2)
+
+
+def unpack_router_logits(router_outputs):
+    total_router_logits = []
+    total_expert_indexes = []
+    for router_output in router_outputs:
+        if len(router_output[0].shape) > 1:
+            router_logits, expert_indexes = router_output
+            total_router_logits.append(router_logits)
+            total_expert_indexes.append(expert_indexes)
+    return torch.cat(total_router_logits, dim=1), torch.cat(total_expert_indexes, dim=1)
+
+
+def switch_router_loss(z_loss_coef, aux_loss_coef, router_outputs):
+    router_logits, expert_indexes = unpack_router_logits(router_outputs)
+    z_loss = router_z_loss_func(router_logits)
+    router_probs = torch.nn.Softmax(dim=-1)(router_logits)
+    aux_loss = load_balancing_loss_func(router_probs, expert_indexes)
+    return z_loss_coef * z_loss + aux_loss_coef * aux_loss
 
 
 class BasicMoe(torch.nn.Module):
@@ -52,12 +101,14 @@ class SwitchMoe(torch.nn.Module):
         self.adapter_name_: str = adapter_name
         self.gate_ = torch.nn.Linear(
             moe_in_features, moe_experts, bias=False, device=device)
+        self.router_z_loss_coef_: float = 0.001
+        self.router_aux_loss_coef_: float = 0.001
         # expert_capacity = (tokens_per_batch / num_experts) * capacity_factor
         self.expert_capacity_: int = moe_expert_capacity
         self.jitter_noise_: float = moe_jitter_noise
         self.experts_: int = moe_experts
 
-    def forward(self, expert_fn, norm_data: torch.Tensor) -> torch.Tensor:
+    def route(self, norm_data: torch.Tensor) -> Tuple:
         if self.jitter_noise_ > 0:
             # Multiply the token inputs by the uniform distribution - adding some noise
             norm_data = norm_data * torch.empty_like(norm_data).uniform_(
@@ -79,14 +130,20 @@ class SwitchMoe(torch.nn.Module):
         expert_index = expert_index * expert_capacity_mask
 
         router_probs = torch.max(router_probs, dim=-1).values.unsqueeze(-1)
+        return expert_index, router_probs, router_logits
+
+    def forward(self, expert_fn, norm_data: torch.Tensor) -> Tuple:
+        router_mask, router_probs, router_logits = self.route(norm_data)
+        expert_index = torch.argmax(router_mask, dim=-1)
 
         next_states = norm_data.clone()
         for idx in range(self.experts_):
-            token_indices = expert_index[:, :, idx].bool()
+            token_indices = router_mask[:, :, idx].bool()
             next_states[token_indices] = expert_fn(
                 self.adapter_name_, idx, norm_data[token_indices])
 
-        return router_probs * next_states
+        hidden_states = router_probs * next_states
+        return hidden_states, (router_logits, expert_index)
 
 
 class MixMLP(torch.nn.Module):
@@ -148,6 +205,15 @@ class MixMLP(torch.nn.Module):
 
             current_hidden_states = self.moes_[
                 moe_name].forward(self._expert_forward, score_norm_data[start_idx:end_idx])
+
+            if isinstance(current_hidden_states, Tuple):
+                current_hidden_states, aux_data = current_hidden_states
+                if lora_config.aux_output_ is None:
+                    lora_config.aux_output_ = (aux_data,)
+                else:
+                    lora_config.aux_output_ = lora_config.aux_output_ + \
+                        (aux_data,)
+
             if final_hidden_states is None:
                 final_hidden_states = current_hidden_states
             else:
