21c07ab5fe290b0fcfdc04c91c30b0ae5b3e7c54
Michael Lee <mikecovlee@163.com>
2023-12-26 11:04:12 +0800

    reduce duplicated codes

--- mlora/model_llama.py
+++ mlora/model_llama.py
@@ -3,7 +3,7 @@ from mlora.checkpoint import CheckpointRecomputeFunction
 from mlora.model import repeat_kv, apply_rotary_emb, precompute_rope_angle, precompute_mask
 from mlora.model import LLMModel, RMSNorm
 from mlora.LoraLiner import Linear
-from mlora.MixLoRA import MixMoe
+from mlora.MixLoRA import MixFFN
 
 import torch
 import torch.nn.functional as F
@@ -65,14 +65,10 @@ class Transformer(torch.nn.Module):
         self.wv_: Linear = None  # dim * dim
         self.wo_: Linear = None  # dim * dim
         # feed forward
-        self.w1_: Linear = None  # also gate FNN * dim
-        self.w2_: Linear = None  # also down dim * FNN
-        self.w3_: Linear = None  # also up   FNN * dim
+        self.ffn_ = MixFFN()
         # norm
         self.attention_norm_: RMSNorm = None  # dim
         self.ffn_norm_: RMSNorm = None        # dim
-        # mix of experts
-        self.moe_: MixMoe = None
         # other arg
         self.layer_id_ = layer_id
         self.norm_eps_ = args.norm_eps_
@@ -81,25 +77,33 @@ class Transformer(torch.nn.Module):
         self.n_rep_ = self.n_heads_ // self.n_kv_heads_
         self.head_dim_ = args.dim_ // args.n_heads_
 
-    def init_lora_layer_weight(self,
-                               adapter_name: str,
-                               r: int,
-                               lora_alpha: int,
-                               lora_dropout: float,
-                               target: Dict[str, bool],
-                               weight: Optional[Dict[str, torch.Tensor]]):
-        linear_layer_list = [self.wk_, self.wq_, self.wv_,
-                             self.wo_, self.w1_, self.w2_, self.w3_]
-        linear_layer_name_list = [
-            "k_proj", "q_proj", "v_proj", "o_proj", "w1_proj", "w2_proj", "w3_proj"]
+    def init_adapter_weight(self, weight: Optional[Dict[str, torch.Tensor]], **kwargs):
+        adapter_type = kwargs.get("adapter_type", "lora")
+        if adapter_type not in ["lora", "mixlora"]:
+            raise f"unkown adapter type {adapter_type}"
+        adapter_name = kwargs["adapter_name"]
+        target = kwargs["target"]
+        if adapter_type == "lora":
+            linear_layer_list = [self.wk_, self.wq_, self.wv_,
+                                 self.wo_, self.ffn_.w1_, self.ffn_.w2_, self.ffn_.w3_]
+            linear_layer_name_list = [
+                "k_proj", "q_proj", "v_proj", "o_proj", "w1_proj", "w2_proj", "w3_proj"]
+        elif adapter_type == "mixlora":
+            linear_layer_list = [self.wk_, self.wq_, self.wv_, self.wo_]
+            linear_layer_name_list = ["k_proj", "q_proj", "v_proj", "o_proj"]
 
         for idx, layer_name in enumerate(linear_layer_name_list):
             if layer_name in target and target[layer_name]:
                 lora_a = None
                 lora_b = None
                 if weight is not None:
-                    lora_a_name = f"base_model.model.model.layers.{self.layer_id_}.self_attn.{layer_name}.lora_A.weight"
-                    lora_b_name = f"base_model.model.model.layers.{self.layer_id_}.self_attn.{layer_name}.lora_B.weight"
+                    if adapter_type == "lora":
+                        lora_a_name = f"base_model.model.model.layers.{self.layer_id_}.self_attn.{layer_name}.lora_A.weight"
+                        lora_b_name = f"base_model.model.model.layers.{self.layer_id_}.self_attn.{layer_name}.lora_B.weight"
+                    elif adapter_type == "mixlora":
+                        lora_a_name = f"mixlora.layers.{self.layer_id_}.self_attn.{layer_name}.lora_A.weight"
+                        lora_b_name = f"mixlora.layers.{self.layer_id_}.self_attn.{layer_name}.lora_B.weight"
+
                     if lora_a_name not in weight:
                         raise f"can not found the layer {lora_a_name} in model"
                     if lora_b_name not in weight:
@@ -108,76 +112,42 @@ class Transformer(torch.nn.Module):
                     lora_b = weight[lora_b_name]
 
                 linear_layer_list[idx].init_lora_weight(
-                    adapter_name, r, lora_alpha, lora_dropout, lora_a, lora_b)
+                    adapter_name, kwargs["lora_r"], kwargs["lora_alpha"], kwargs["lora_dropout"], lora_a, lora_b)
 
-    def init_mixlora_layer_weight(self,
-                                  moe_name: str,
-                                  lora_r: int,
-                                  lora_alpha: int,
-                                  lora_dropout: float,
-                                  moe_experts: int,
-                                  moe_topk: int,
-                                  target: Dict[str, bool],
-                                  weight: Optional[torch.Tensor],
-                                  ):
-        # Inject LoRA configs into Attention Layer
-        attention_layer_list = [self.wk_, self.wq_, self.wv_, self.wo_]
-        attention_layer_name_list = ["k_proj", "q_proj", "v_proj", "o_proj"]
+        if adapter_type == "mixlora":
+            # Inject LoRA configs into FFN layer
+            if weight is not None:
+                self.ffn_.init_moe_weight(adapter_name, self.n_heads_ * self.head_dim_, kwargs["moe_experts"],
+                                          kwargs["moe_topk"], weight[f"mixlora.layers.{self.layer_id_}.gate.weight"])
+            else:
+                self.ffn_.init_moe_weight(
+                    adapter_name, self.n_heads_ * self.head_dim_, kwargs["moe_experts"], kwargs["moe_topk"], None)
 
-        for idx, layer_name in enumerate(attention_layer_name_list):
-            if layer_name in target and target[layer_name]:
-                lora_a = None
-                lora_b = None
-                if weight is not None:
-                    lora_a_name = f"mixlora.layers.{self.layer_id_}.self_attn.{layer_name}.lora_A.weight"
-                    lora_b_name = f"mixlora.layers.{self.layer_id_}.self_attn.{layer_name}.lora_B.weight"
-                    if lora_a_name not in weight:
-                        raise f"can not found the layer {lora_a_name} in model"
-                    if lora_b_name not in weight:
-                        raise f"can not found the layer {lora_b_name} in model"
-                    lora_a = weight[lora_a_name]
-                    lora_b = weight[lora_b_name]
+            ffn_layer_list = [self.ffn_.w1_, self.ffn_.w2_, self.ffn_.w3_]
+            ffn_layer_name_list = ["w1_proj", "w2_proj", "w3_proj"]
+            for idx, layer_name in enumerate(ffn_layer_name_list):
+                if layer_name in target and target[layer_name]:
+                    for expert_idx in range(self.ffn_.moes_[adapter_name].experts_):
+                        lora_a = None
+                        lora_b = None
+                        if weight is not None:
+                            lora_a_name = f"mixlora.layers.{self.layer_id_}.experts.{expert_idx}.{layer_name}.lora_A.weight"
+                            lora_b_name = f"mixlora.layers.{self.layer_id_}.experts.{expert_idx}.{layer_name}.lora_B.weight"
+                            if lora_a_name not in weight:
+                                raise f"can not found the layer {lora_a_name} in model"
+                            if lora_b_name not in weight:
+                                raise f"can not found the layer {lora_b_name} in model"
+                            lora_a = weight[lora_a_name]
+                            lora_b = weight[lora_b_name]
 
-                attention_layer_list[idx].init_lora_weight(
-                    moe_name, lora_r, lora_alpha, lora_dropout, lora_a, lora_b
-                )
-
-        # Inject LoRA configs into FFN layer
-        if self.moe_ is None:
-            self.moe_ = MixMoe(self.w1_, self.w2_, self.w3_)
-
-        if weight is not None:
-            self.moe_.init_moe_weight(moe_name, self.n_heads_ * self.head_dim_, moe_experts,
-                                      moe_topk, weight[f"mixlora.layers.{self.layer_id_}.gate.weight"])
-        else:
-            self.moe_.init_moe_weight(
-                moe_name, self.n_heads_ * self.head_dim_, moe_experts, moe_topk, None)
-
-        ffn_layer_list = [self.w1_, self.w2_, self.w3_]
-        ffn_layer_name_list = ["w1_proj", "w2_proj", "w3_proj"]
-        for idx, layer_name in enumerate(ffn_layer_name_list):
-            if layer_name in target and target[layer_name]:
-                for expert_idx in range(self.moe_.moes_[moe_name].experts_):
-                    lora_a = None
-                    lora_b = None
-                    if weight is not None:
-                        lora_a_name = f"mixlora.layers.{self.layer_id_}.experts.{expert_idx}.{layer_name}.lora_A.weight"
-                        lora_b_name = f"mixlora.layers.{self.layer_id_}.experts.{expert_idx}.{layer_name}.lora_B.weight"
-                        if lora_a_name not in weight:
-                            raise f"can not found the layer {lora_a_name} in model"
-                        if lora_b_name not in weight:
-                            raise f"can not found the layer {lora_b_name} in model"
-                        lora_a = weight[lora_a_name]
-                        lora_b = weight[lora_b_name]
-
-                    ffn_layer_list[idx].init_lora_weight(
-                        f"moe.{moe_name}.experts.{expert_idx}",
-                        lora_r,
-                        lora_alpha,
-                        lora_dropout,
-                        lora_a,
-                        lora_b,
-                    )
+                        ffn_layer_list[idx].init_lora_weight(
+                            f"moe.{adapter_name}.experts.{expert_idx}",
+                            kwargs["lora_r"],
+                            kwargs["lora_alpha"],
+                            kwargs["lora_dropout"],
+                            lora_a,
+                            lora_b,
+                        )
 
     # @torch.compile
     def forward(self,
@@ -216,16 +186,7 @@ class Transformer(torch.nn.Module):
 
         # feed forward fully connected
         score_norm_data = self.ffn_norm_.forward(data)
-
-        # Switching to a dedicated MoE FFN layer when available
-        if self.moe_ is not None:
-            final_ffn_output = self.moe_.forward(score_norm_data, input_args)
-        else:
-            w1 = self.w1_.forward(score_norm_data, input_args)
-            w3 = self.w3_.forward(score_norm_data, input_args)
-            final_ffn_output = self.w2_.forward(F.silu(w1) * w3, input_args)
-
-        data = data + final_ffn_output
+        data = data + self.ffn_.forward(score_norm_data, input_args)
 
         return data
 
@@ -307,26 +268,8 @@ class LlamaModel(LLMModel):
 
     def init_adapter_weight(self, weight: Optional[Dict[str, torch.Tensor]], **kwargs):
         self.adapter_type_ = kwargs.get("adapter_type", "lora")
-        if self.adapter_type_ == "lora":
-            for transformer_layer in self.layers_:
-                transformer_layer.init_lora_layer_weight(
-                    kwargs["adapter_name"],
-                    kwargs["lora_r"],
-                    kwargs["lora_alpha"],
-                    kwargs["lora_dropout"],
-                    kwargs["target"], weight)
-        elif self.adapter_type_ == "mixlora":
-            for transformer_layer in self.layers_:
-                transformer_layer.init_mixlora_layer_weight(
-                    kwargs["adapter_name"],
-                    kwargs["lora_r"],
-                    kwargs["lora_alpha"],
-                    kwargs["lora_dropout"],
-                    kwargs["moe_experts"],
-                    kwargs["moe_topk"],
-                    kwargs["target"], weight)
-        else:
-            raise f"unkown adapter type {self.adapter_type_}"
+        for transformer_layer in self.layers_:
+            transformer_layer.init_adapter_weight(weight, **kwargs)
 
     def from_pretrained(path: str,
                         device: str,
@@ -400,9 +343,12 @@ class LlamaModel(LLMModel):
                 layer.self_attn.v_proj, device=device)
             model.layers_[idx].wo_ = Linear(
                 layer.self_attn.o_proj, device=device)
-            model.layers_[idx].w1_ = Linear(layer.mlp.gate_proj, device=device)
-            model.layers_[idx].w2_ = Linear(layer.mlp.down_proj, device=device)
-            model.layers_[idx].w3_ = Linear(layer.mlp.up_proj, device=device)
+            model.layers_[idx].ffn_.w1_ = Linear(
+                layer.mlp.gate_proj, device=device)
+            model.layers_[idx].ffn_.w2_ = Linear(
+                layer.mlp.down_proj, device=device)
+            model.layers_[idx].ffn_.w3_ = Linear(
+                layer.mlp.up_proj, device=device)
             model.layers_[idx].attention_norm_ = RMSNorm(
                 layer.input_layernorm.weight.to(device=device).requires_grad_(False), model.norm_eps_)
             model.layers_[idx].ffn_norm_ = RMSNorm(
@@ -421,8 +367,8 @@ class LlamaModel(LLMModel):
 
                 lora_layer_list = [transformer_layer.wq_.loras_, transformer_layer.wk_.loras_,
                                    transformer_layer.wv_.loras_, transformer_layer.wo_.loras_,
-                                   transformer_layer.w1_.loras_, transformer_layer.w2_.loras_,
-                                   transformer_layer.w3_.loras_]
+                                   transformer_layer.ffn_.w1_.loras_, transformer_layer.ffn_.w2_.loras_,
+                                   transformer_layer.ffn_.w3_.loras_]
 
                 for lora_layer in lora_layer_list:
                     if adapter_name in lora_layer:
@@ -430,7 +376,7 @@ class LlamaModel(LLMModel):
                             lora_layer[adapter_name].lora_a_)
                         train_paramas[adapter_name].append(
                             lora_layer[adapter_name].lora_b_)
-                    elif transformer_layer.moe_ is not None:
+                    elif transformer_layer.ffn_.enable_moe_:
                         for expert_idx in range(lora_config["experts"]):
                             lora_name = f"moe.{adapter_name}.experts.{expert_idx}"
                             if lora_name in lora_layer:
@@ -455,8 +401,8 @@ class LlamaModel(LLMModel):
 
             lora_layer_list = [transformer_layer.wq_, transformer_layer.wk_,
                                transformer_layer.wv_, transformer_layer.wo_,
-                               transformer_layer.w1_, transformer_layer.w2_,
-                               transformer_layer.w3_]
+                               transformer_layer.ffn_.w1_, transformer_layer.ffn_.w2_,
+                               transformer_layer.ffn_.w3_]
             lora_layer_name_list = [
                 "q_proj", "k_proj", "v_proj", "o_proj", "w1_proj", "w2_proj", "w3_proj"]
             for idx, lora_layer in enumerate(lora_layer_list):
@@ -467,9 +413,9 @@ class LlamaModel(LLMModel):
                                      f"{lora_layer_name_list[idx]}.lora_A.weight"] = lora_layer.loras_[lora_name].lora_a_
                     lora_weight_dict[layer_prefix_name +
                                      f"{lora_layer_name_list[idx]}.lora_B.weight"] = lora_layer.loras_[lora_name].lora_b_
-                elif transformer_layer.moe_ is not None:
+                elif transformer_layer.ffn_.enable_moe_:
                     moe_layer_prefix_name = f"mixlora.layers.{transformer_layer.layer_id_}."
-                    for expert_idx in range(transformer_layer.moe_.moes_[lora_name].experts_):
+                    for expert_idx in range(transformer_layer.ffn_.moes_[lora_name].experts_):
                         moe_lora_name = f"moe.{lora_name}.experts.{expert_idx}"
                         if moe_lora_name in lora_layer.loras_:
                             if lora_layer_name_list[idx] not in target_modules:
@@ -488,7 +434,7 @@ class LlamaModel(LLMModel):
 
                     lora_weight_dict[
                         moe_layer_prefix_name + "gate.weight"
-                    ] = transformer_layer.moe_.moes_[lora_name].gate_.weight
+                    ] = transformer_layer.ffn_.moes_[lora_name].gate_.weight
 
         return lora_weight_dict, target_modules
 
