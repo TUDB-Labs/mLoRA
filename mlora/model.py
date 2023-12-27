43e4318b355a238d5432ae84eacf3daa02ecea0f
Michael Lee <mikecovlee@163.com>
2023-12-27 22:43:44 +0800

    try fix issues of inference

--- mlora/model.py
+++ mlora/model.py
@@ -102,25 +102,23 @@ class RMSNorm(torch.nn.Module):
 
 class KVCache:
     def __init__(self) -> None:
-        self.key_cache: List[torch.Tensor] = []
-        self.value_cache: List[torch.Tensor] = []
+        self.cache_k: List[torch.Tensor] = []
+        self.cache_v: List[torch.Tensor] = []
+        self.seq_pos: int = 0
 
-    def update(
-        self,
-        key_states: torch.Tensor,
-        value_states: torch.Tensor,
-        layer_idx: int,
-    ) -> Tuple[torch.Tensor, torch.Tensor]:
-        if len(self.key_cache) <= layer_idx:
-            self.key_cache.append(key_states)
-            self.value_cache.append(value_states)
+    def update(self, xk: torch.Tensor, xv: torch.Tensor, layer_idx: int,
+               bsz: int, seq_len: int) -> Tuple[torch.Tensor, torch.Tensor]:
+        if len(self.cache_k) <= layer_idx:
+            self.cache_k.append(xk)
+            self.cache_v.append(xv)
         else:
-            self.key_cache[layer_idx] = torch.cat(
-                [self.key_cache[layer_idx], key_states], dim=1)
-            self.value_cache[layer_idx] = torch.cat(
-                [self.value_cache[layer_idx], value_states], dim=1)
+            self.cache_k[layer_idx][:bsz,
+                                    self.seq_pos: self.seq_pos + seq_len] = xk
+            self.cache_v[layer_idx][:bsz,
+                                    self.seq_pos: self.seq_pos + seq_len] = xv
 
-        return self.key_cache[layer_idx], self.value_cache[layer_idx]
+        return self.cache_k[layer_idx][:bsz, :self.seq_pos + seq_len], \
+            self.cache_v[layer_idx][:bsz, :self.seq_pos + seq_len]
 
 
 class LLMModel(metaclass=ABCMeta):
