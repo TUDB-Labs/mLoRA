6c70379ff7148524f1bfc60c1f133f3aec1f95fe
Michael Lee <mikecovlee@163.com>
2024-02-05 20:15:00 +0800

    add arc tests

---
+++ arc/prepare_data.py
@@ -0,0 +1,24 @@
+import datasets
+import json
+
+
+def prep_data(data, file_path):
+    json_data = []
+    for data_point in data:
+        obj = {}
+        obj["instruction"] = data_point["question"]
+        choices_str = ""
+        choices = data_point["choices"]
+        for label, text in zip(choices["label"], choices["text"]):
+            choices_str += f"\n{label}: {text}"
+        obj["input"] = choices_str
+        obj["output"] = data_point["answerKey"]
+        json_data.append(obj)
+    with open(file_path, "w") as f:
+        json.dump(json_data, f, indent=4)
+
+
+arc_data = datasets.load_dataset("allenai/ai2_arc", "ARC-Challenge")
+
+prep_data(arc_data["train"], "./data_train.json")
+# prep_data(arc_data["test"], "./data_test.json")
