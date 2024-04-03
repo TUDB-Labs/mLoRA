deae3da9976c172eae6a56cdd980767640dc5aac
Mike Lee <mikecovlee@163.com>
2024-04-04 04:15:26 +0800

    sync linux requirements

--- install_linux.sh
+++ install_linux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Install extra requirements
 pip install ninja
-pip install xformers==0.0.24
+pip install xformers==0.0.23.post1
 pip install bitsandbytes==0.41.3
 pip install flash-attn==2.5.6 --no-build-isolation
