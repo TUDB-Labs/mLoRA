2a907d972acc2bc2347c5b2647e7eed2aa76dec0
Leon yuan <113282807+Leonyuan1@users.noreply.github.com>
2024-04-07 21:08:44 +0800

    Add files via upload

---
+++ tests/test_tokenizer.py
@@ -0,0 +1,80 @@
+"""
+This is a test file for the tokenizer.py module.
+"""
+
+import os
+import sys
+import unittest
+from unittest.mock import MagicMock
+from mlora.tokenizer import Tokenizer
+
+CURRENT_DIR = os.path.split(os.path.abspath(__file__))[0]
+config_path = CURRENT_DIR.rsplit('/', 1)[0]
+sys.path.append(config_path)
+
+
+class TestTokenizer(unittest.TestCase):
+    """
+    Test cases for the Tokenizer class.
+    """
+    def setUp(self):
+        """
+        Set up method.
+        """
+        # Create a mock instance of Tokenizer using MagicMock
+        self.tokenizer = MagicMock(spec=Tokenizer)
+
+    def test_encode(self):
+        """
+        Test the encode method.
+        """
+        test_cases = [
+            # Test cases for encode method
+            ("hello world", True, True, [1, 22172, 3186, 2]),
+            ("hello world", False, False, [22172, 3186]),
+            ("apple", False, True, [26163, 2]),
+            ("apple", True, False, [1, 26163]),
+            ("apple", None, None, [1, 26163]),  # Default values for bos and eos
+        ]
+        for input_str, bos, eos, expected_output in test_cases:
+            with self.subTest(input_str=input_str, bos=bos,
+                              eos=eos, expected_output=expected_output):
+                # Set the return value for the mock encode method
+                self.tokenizer.encode.return_value = expected_output
+                self.assertEqual(self.tokenizer.encode(input_str, bos=bos, eos=eos),
+                                expected_output)
+
+    def test_decode(self):
+        """
+        Test the decode method.
+        """
+        test_cases = [
+            # Test cases for decode method
+            ([22172, 3186], "hello world"),
+            ([26163], "apple"),
+        ]
+
+        for input_list, expected_output in test_cases:
+            with self.subTest(input_list=input_list, expected_output=expected_output):
+                # Set the return value for the mock decode method
+                self.tokenizer.decode.return_value = expected_output
+                self.assertEqual(self.tokenizer.decode(input_list), expected_output)
+
+    def test_mask_from(self):
+        """
+        Test the mask_from method.
+        """
+        test_cases = [
+            # Test cases for mask_from method
+            ([0, 31414, 232, 328, 2005, 7], [1, 0, 0, 0, 0, 0]),
+            ([31414, 232, 328, 2005, 7], [0, 0, 0, 0, 0]),
+        ]
+        for input_list, expected_output in test_cases:
+            with self.subTest(input_list=input_list, expected_output=expected_output):
+                # Set the return value for the mock mask_from method
+                self.tokenizer.mask_from.return_value = expected_output
+                self.assertEqual(self.tokenizer.mask_from(input_list), expected_output)
+
+
+if __name__ == '__main__':
+    unittest.main()
