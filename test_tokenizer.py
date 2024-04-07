ada59e0cc079cf41bbea5e428372aaa47515f03a
Leon yuan <113282807+Leonyuan1@users.noreply.github.com>
2024-04-07 17:05:42 +0800

    Update test_tokenizer.py

--- test_tokenizer.py
+++ test_tokenizer.py
@@ -4,6 +4,7 @@ from mlora.tokenizer import Tokenizer
 
 
 class TestTokenizer(unittest.TestCase):
+    
     def setUp(self):
         # Create a mock instance of Tokenizer using MagicMock
         self.tokenizer = MagicMock(spec=Tokenizer)
@@ -17,7 +18,6 @@ class TestTokenizer(unittest.TestCase):
             ("apple", True, False, [1, 26163]),
             ("apple", None, None, [1, 26163]),  # Default values for bos and eos
         ]
-        
         for input_str, bos, eos, expected_output in test_cases:
             with self.subTest(input_str=input_str, bos=bos, eos=eos, expected_output=expected_output):
                 # Set the return value for the mock encode method
@@ -30,26 +30,24 @@ class TestTokenizer(unittest.TestCase):
             ([22172, 3186], "hello world"),
             ([26163], "apple"),
         ]
-        
         for input_list, expected_output in test_cases:
             with self.subTest(input_list=input_list, expected_output=expected_output):
                 # Set the return value for the mock decode method
                 self.tokenizer.decode.return_value = expected_output
                 self.assertEqual(self.tokenizer.decode(input_list), expected_output)
-
+                
     def test_mask_from(self):
         test_cases = [
             # Test cases for mask_from method
             ([0, 31414, 232, 328, 2005, 7], [1, 0, 0, 0, 0, 0]),
             ([31414, 232, 328, 2005, 7], [0, 0, 0, 0, 0]),
         ]
-        
         for input_list, expected_output in test_cases:
             with self.subTest(input_list=input_list, expected_output=expected_output):
                 # Set the return value for the mock mask_from method
                 self.tokenizer.mask_from.return_value = expected_output
                 self.assertEqual(self.tokenizer.mask_from(input_list), expected_output)
 
-
 if __name__ == '__main__':
+    
     unittest.main()
