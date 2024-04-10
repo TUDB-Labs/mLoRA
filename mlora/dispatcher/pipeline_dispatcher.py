dad5671b5df2f06e5b70af44926e3c9f5df86f65
Vinkle-hzt <vinkle-hzt@outlook.com>
2024-04-10 06:43:02 +0000

    feature: support micro_batch in pipeline

--- mlora/dispatcher/pipeline_dispatcher.py
+++ mlora/dispatcher/pipeline_dispatcher.py
@@ -6,23 +6,32 @@ from typing import Dict, List
 
 
 class PipelineDispatcher(Dispatcher):
-    _adapter_lock_: Dict[str, bool] = {}
+    _adapter_backward_cnt_: Dict[str, int] = {}
+    _adapter_forward_cnt_: Dict[str, int] = {}
+    _adapter_accumulation_step_: Dict[str, int] = {}
 
     def __init__(self,
                  config: MLoRAConfig,
                  tokenizer: Tokenizer) -> None:
         super().__init__(config, tokenizer)
+        for lora_config in config.lora_configs_:
+            adapter_name = lora_config.adapter_name_
+            accumulation_step = lora_config.batch_size_ / lora_config.micro_batch_size_
+            self._adapter_forward_cnt_[adapter_name] = 0
+            self._adapter_backward_cnt_[adapter_name] = 0
+            self._adapter_accumulation_step_[adapter_name] = accumulation_step
 
-    def activate_adapter(self, adapter_name: str):
-        self._adapter_lock_[adapter_name] = True
+    def update_backward_cnt(self, adapter_name: str):
+        self._adapter_backward_cnt_[adapter_name] += 1
+        if self._adapter_backward_cnt_[adapter_name] == self._adapter_accumulation_step_[adapter_name]:
+            self._adapter_forward_cnt_[adapter_name] = 0
+            self._adapter_backward_cnt_[adapter_name] = 0
 
-    def deactivate_adapter(self, adapter_name: str):
-        self._adapter_lock_[adapter_name] = False
+    def update_forward_cnt(self, adapter_name: str):
+        self._adapter_forward_cnt_[adapter_name] += 1
 
     def __check_adapter_available(self, adapter_name: str) -> bool:
-        if adapter_name in self._adapter_lock_:
-            return self._adapter_lock_[adapter_name]
-        return True
+        return self._adapter_forward_cnt_[adapter_name] < self._adapter_accumulation_step_[adapter_name]
 
     def rigister_strategies(self):
         self.rigister_strategy("pipe", self.pipe_dispatch_strategy)
@@ -36,7 +45,7 @@ class PipelineDispatcher(Dispatcher):
             # check the adapter is available
             if not self.__check_adapter_available(task.adapter_name_):
                 continue
-            self.deactivate_adapter(task.adapter_name_)
+            self.update_forward_cnt(task.adapter_name_)
             ret_train_data[task.adapter_name_] = task.get_train_data()
             cnt += 1
             if cnt >= self.train_lora_simultaneously_num_:
