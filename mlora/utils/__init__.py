970a866376903733c9c65bacf9eb2c59914f403f
Mike Lee <mikecovlee@163.com>
2024-07-07 16:24:57 +0800

    add nonecontext

--- mlora/utils/__init__.py
+++ mlora/utils/__init__.py
@@ -1,6 +1,7 @@
 from .cmd import get_cmd_args, get_server_cmd_args
 from .loader import load_model
 from .setup import setup_cuda_check, setup_logging, setup_seed, setup_trace_mode
+from .package import is_package_available, Linear8bitLt, Linear4bit, BitsAndBytesConfig, NoneContexts
 
 __all__ = [
     "get_cmd_args",
@@ -10,4 +11,9 @@ __all__ = [
     "setup_logging",
     "setup_cuda_check",
     "setup_trace_mode",
+    "is_package_available",
+    "Linear8bitLt",
+    "Linear4bit",
+    "BitsAndBytesConfig",
+    "NoneContexts",
 ]
