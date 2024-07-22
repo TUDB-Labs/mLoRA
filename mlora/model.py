ba52ecd3890e73c3a50c24e8d50b803a9b9aefc8
Mike Lee <mikecovlee@163.com>
2024-07-22 13:26:46 +0800

    fix attention mask

--- mlora/model.py
+++ mlora/model.py
@@ -286,7 +286,7 @@ class LLMModel(torch.nn.Module):
         )
 
         # prepare mask
-        if input_args.batch_masks_ is not None and 0 in input_args.batch_masks_:
+        if input_args.batch_masks_ is not None and 1 in input_args.batch_masks_:
             # 2d mask is passed through the layers
             if isinstance(input_args.batch_masks_, torch.Tensor):
                 attention_mask = input_args.batch_masks_.to(
