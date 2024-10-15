cfe564a3d67b43771c14685da5693c4de33c4ee2
junshijun <475216305@qq.com>
2024-10-15 22:48:12 +0800

    add model

--- mlora/model/llm/__init__.py
+++ mlora/model/llm/__init__.py
@@ -1,4 +1,5 @@
 from .model_llama import LlamaModel
 from .model_llm import LLMModel
+from .model_chatglm import ChatglmModel
 
-__all__ = ["LLMModel", "LlamaModel"]
+__all__ = ["LLMModel", "LlamaModel", "ChatglmModel"]
